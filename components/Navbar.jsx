a8ce224ce316507600667eab239f6dba4a2afdc5
Hasan <hasanizmirr@gmail.com>
2023-03-08 17:29:22 +0300

    improvement: Navbar component updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,12 +4,14 @@ import { GiBookshelf } from "react-icons/gi"
 
 const Navbar = () => {
   return (
-    <div className="w-full py-6 flex justify-between px-5">
-      <Link href="/" className="font-semibold nav-link">Books</Link>
-      <GiBookshelf className="text-3xl text-red-500"/>
+    <div className="max-w-[1240px] m-auto py-6 flex justify-between px-5">
+      <nav>
+        <Link href="/" className="font-semibold nav-link">Books</Link>
+        <Link href="/contact" className="font-semibold nav-link">Contact</Link>
+      </nav>
       <button className="flex text-2xl cursor-pointer relative border-0">
         <AiOutlineShopping className="nav-link"/>
-        <span className="absolute -right-2 text-xs w-4 h-4 rounded-full bg-red-600 text-white">
+        <span className="absolute right-3 text-xs w-4 h-4 rounded-full bg-red-600 text-white">
           4
         </span>
       </button>
