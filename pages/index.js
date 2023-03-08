163e4245dde983a23c04a1871c43712f7c5d5d71
Hasan <hasanizmirr@gmail.com>
2023-03-08 18:38:25 +0300

    feat: Home page updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-export default function Home() {
+import Banner from "@/components/Banner";
+import ProductItem from "@/components/ProductItem";
+import axios from "axios";
+
+export default function Home({ data }) {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="px-5">
+      <Banner />
+      <div className="flex flex-wrap gap-5 mt-10">
+        {data.books?.map((product) => (
+          <ProductItem key={product.isbn13} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get("https://api.itbook.store/1.0/new");
+
+  return {
+    props: { data },
+  };
+};
